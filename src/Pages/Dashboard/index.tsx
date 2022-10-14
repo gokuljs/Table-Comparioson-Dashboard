149a2d6a1ba9290a317323db766838ac5bbfce70
Gokul Js <gokuljs029@gmail.com>
2022-10-14 19:57:18 +0530

    drodown fix

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,19 +1,44 @@
 import { Grid } from "@mui/material";
 import React, { useState } from "react";
 import TableCard from "../../Components/cards/TableCard";
-import { DashboardWrapper } from "./styles";
+import { DashboardWrapper, DifferentCriteria } from "./styles";
 import { DummyDatasetProps } from "./types";
 import { dummyDataset as dummyData } from "./utils";
+import ArrowDropUpTwoToneIcon from "@mui/icons-material/ArrowDropUpTwoTone";
+import ArrowDropDownTwoToneIcon from "@mui/icons-material/ArrowDropDownTwoTone";
 
 function Dashboard() {
   const [dummyDataSet, setDummyDataSet] =
     useState<DummyDatasetProps[]>(dummyData);
   const [showDetailedFundingHistory, setShowDetailedFundingHistory] =
     useState<boolean>(false);
+  const [showCriteria, setShowCriteria] = useState(false);
 
   return (
     <DashboardWrapper>
-      <div className="add-criteria">Add criteria</div>
+      <div className="add-criteria">
+        <div
+          className="add-criteria-text"
+          onClick={() => {
+            setShowCriteria(!showCriteria);
+          }}
+        >
+          <Grid display="flex" alignItems="center">
+            Add criteria{" "}
+            {showCriteria ? (
+              <ArrowDropUpTwoToneIcon />
+            ) : (
+              <ArrowDropDownTwoToneIcon />
+            )}
+          </Grid>
+          <DifferentCriteria showCriteria={showCriteria}>
+            <Grid className="criteria-list">Company Info</Grid>
+            <Grid className="criteria-list">Features</Grid>
+            <Grid className="criteria-list">Customer Cases Studies</Grid>
+          </DifferentCriteria>
+        </div>
+      </div>
+
       <Grid display="flex" flexWrap="nowrap">
         {Array.isArray(dummyDataSet) &&
           dummyDataSet.map((value) => (
--- src/Pages/Dashboard/styles.tsx
+++ src/Pages/Dashboard/styles.tsx
@@ -13,4 +13,32 @@ export const DashboardWrapper = styled(Grid)`
     text-transform: uppercase;
     font-size: 0.725rem;
   }
+  .add-criteria-text {
+    position: relative;
+    cursor: pointer;
+  }
 `;
+
+export const DifferentCriteria = styled(Grid)(
+  ({ showCriteria }: { showCriteria: boolean }) => ({
+    borderLeft: "2px solid #d6dbd8",
+    borderRight: "2px solid #d6dbd8",
+    borderRadius: "8px",
+    position: "absolute",
+    zIndex: "9",
+    display: showCriteria ? "block" : "none",
+    background: "#fff",
+    minWidth: "7rem",
+    left: "1rem",
+    padding: 0,
+    cursor: "pointer",
+    top: "1.5rem",
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+    ".criteria-list": {
+      padding: "0.8rem 0.725rem",
+      borderTop: "0.5px solid #d6dbd8",
+      borderRadius: "3px",
+      margin: 0,
+    },
+  })
+);
